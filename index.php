316131e773b7f9aa6539bddf1f08be36d0b345b3
Falco Hospes <61621@student.drenthecollege.nl>
2019-03-29 10:42:04 +0100

    Eerste Commit
    Bevat de basis onderdelen van het Forum, zoals CSS, JS, Index.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+		
+<head>
+		<meta charset="UTF-8">
+		<meta name="description" content="Scrum Opdracht DG1">
+		<meta name="keywords" content="">
+		<meta name="author" content="Falco Hospes">
+		
+		<title>Het Forum</title>
+		<link rel="stylesheet" href="css\forum.css">
+		<link rel="stylesheet" href="css\bootstrap.css">
+		
+
+</head>
+<body>
+
+
+
+</body>
+
+</html>
